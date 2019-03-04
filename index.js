859b6bded1743b50e8080eff20fe4f8596706fe6
Jose Torres <jose.torres@efika.solutions>
2019-03-04 14:01:15 -0600

    Add first module

---
+++ index.js
@@ -0,0 +1,22 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+ReactDOM.render(React.createElement(
+    "ul",
+    null,
+    React.createElement(
+        "li",
+        null,
+        "I am"
+    ),
+    React.createElement(
+        "li",
+        null,
+        "Learning"
+    ),
+    React.createElement(
+        "li",
+        null,
+        "React"
+    )
+), document.getElementById("root"));
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import  React  from "react"
+import ReactDOM  from "react-dom"
+
+ReactDOM.render(
+    <ul>
+        <li>I am</li>
+        <li>Learning</li>
+        <li>React</li>
+    </ul>, document.getElementById("root"))
